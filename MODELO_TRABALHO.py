1a71971ed8a09072d90481ef32bb466217a0ae10
MathScheffer <matheusmartinsscheffer@hotmail.com>
2022-10-17 18:26:58 -0300

    inicializar repositório

---
+++ MODELO_TRABALHO.py
@@ -0,0 +1,18 @@
+########################################################
+# Disciplina: Análise de Dados
+###
+# Trabalho 1
+# NOME: <informe seu nome>
+########################################################
+
+# Número da Tarefa
+script
+
+# Número da Tarefa
+script
+
+# Número da Tarefa
+script
+
+# Número da Tarefa
+script
